d4d3c963d00dee6c8dc76a1582b4392e9fd04ab3
Alessandro Maroso <alessandro.maroso@gmail.com>
2014-01-28 15:56:41 +0100

    First commit

---
+++ SXPhysics/SXPhysics/SXPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SXPAppDelegate.h
+//  SXPhysics
+//
+//  Created by Alessandro Maroso on 28/01/14.
+//  Copyright (c) 2014 waresme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SXPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SXPhysics/SXPhysicsTests/SXPhysicsTests.m
@@ -0,0 +1,34 @@
+//
+//  SXPhysicsTests.m
+//  SXPhysicsTests
+//
+//  Created by Alessandro Maroso on 28/01/14.
+//  Copyright (c) 2014 waresme. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SXPhysicsTests : XCTestCase
+
+@end
+
+@implementation SXPhysicsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
