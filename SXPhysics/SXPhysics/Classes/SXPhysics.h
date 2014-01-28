788a542c4d82601ccba66ddda49547afcb847731
Alessandro Maroso <alessandro.maroso@gmail.com>
2014-01-28 16:03:53 +0100

    Added library header file.

---
+++ SXPhysics/SXPhysics/Classes/SXPhysics.h
@@ -0,0 +1,11 @@
+//
+//  SXPhysics.h
+//  SXPhysics
+//
+//  Created by Alessandro Maroso on 28/01/14.
+//  Copyright (c) 2014 waresme. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// TODO: Add all the headers.
