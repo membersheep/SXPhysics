14a1805287ed43a217e7a04002496eb6b2e8964c
Alessandro Maroso <alessandro.maroso@gmail.com>
2014-10-31 18:51:33 +0100

    [Game.mm] Added touch joint example

--- SXPhysics/SXPhysicsScaffold/Game.mm
+++ SXPhysics/SXPhysicsScaffold/Game.mm
@@ -7,6 +7,7 @@
 //
 
 #import "Game.h"
+#import "SXTouchJoint.h"
 
 @interface Game ()
 
@@ -14,6 +15,7 @@
 @property (nonatomic, strong) SXBody* ground;
 @property (nonatomic, strong) SXBody* sparrow;
 @property (nonatomic, strong) SXBody* hotDog;
+@property (nonatomic, strong) SXTouchJoint* touchJoint;
 
 @end
 
@@ -72,13 +74,73 @@
     
 }
 
+//-(void)onTouch:(SPTouchEvent*)event
+//{
+//    SPTouch *touch = [event.touches anyObject];
+//    if (touch.phase == SPTouchPhaseEnded)
+//    {
+//        SPPoint *touchLocation = [touch locationInSpace:self.sparrow];
+//        [self.sparrow setVelocity:CGPointMake(touchLocation.x, touchLocation.y)];
+//    }
+//}
+
 -(void)onTouch:(SPTouchEvent*)event
 {
-    SPTouch *touch = [event.touches anyObject];
+    SPTouch *touch = [[event touchesWithTarget:self] anyObject];
+    SPPoint *position = [touch locationInSpace:self];
+    if (touch.phase == SPTouchPhaseBegan)
+    {
+        NSLog(@"SPWorld touch position: %@", [position description]);
+        NSLog(@"Touched object class: %@", [touch.target class]);
+    }
+    
+    // TouchJoint test
+    if (touch.phase == SPTouchPhaseBegan)
+    {
+        // find if a body was touched
+        self.touchJoint.target = CGPointMake(position.x, position.y);
+        id touchTarget = touch.target;
+        while( ![touchTarget isKindOfClass:[SXBody class]] )
+        {
+            touchTarget = [touchTarget parent];
+            if (touchTarget)
+            {
+                if([touchTarget isKindOfClass:[Game class]] )
+                    break;
+            }
+            else break;
+        }
+        // if i found a body, create a touch joint to move it
+        if([touchTarget isKindOfClass:[SXBody class]])
+        {
+            SXBody *touchBody = touchTarget;
+            if ([touchBody.name isEqualToString:@"ground"])
+                NSLog(@"creating joint");
+            // create mouse touch joint (instance variable)
+            self.touchJoint = [SXTouchJoint touchJoint];
+            self.touchJoint.bodyA = self.ground;
+            self.touchJoint.bodyB = touchBody;
+            // activate the collision with the reference body (the ground)
+            self.touchJoint.collideConnected = YES;
+            // the max force that will affect other bodies after collisions with the box
+            self.touchJoint.maxForce = 50;
+            // specify the initial target position (in world coordinates)
+            self.touchJoint.target = CGPointMake(touchBody.x, touchBody.y);
+            [self.world addChild:self.touchJoint];
+            NSLog(@"joint created");
+        }
+    }
+    if (touch.phase == SPTouchPhaseMoved)
+    {
+        // when there is a touch movement the target should be moved
+        if(self.touchJoint)
+            self.touchJoint.target = CGPointMake(position.x, position.y);
+    }
     if (touch.phase == SPTouchPhaseEnded)
     {
-        SPPoint *touchLocation = [touch locationInSpace:self.sparrow];
-        [self.sparrow setVelocity:CGPointMake(touchLocation.x, touchLocation.y)];
+        // remove the touch joint when the touch ends
+        if(self.touchJoint)
+            [self.world removeChild:self.touchJoint];
     }
 }
 
